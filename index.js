3e4af684a138f7ed28d50d97d0da1e7c143f038d
Orekoya <tundeorekoya1998@gmail.com>
2023-05-22 00:26:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const symbol = "!@#$%^&*()_+";
+const number = "0123456789";
+
+const allchars = upperCase + lowerCase + symbol + number;
+
+function createPassword(){
+let password = "";
+
+password += upperCase[Math.floor(Math.random() * upperCase.length)]; 
+password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+password += symbol[Math.floor(Math.random() * symbol.length)];
+password += number[Math.floor(Math.random() * number.length)];
+
+
+while(length > password.length){
+  
+  password += allchars[Math.floor(Math.random() * allchars.length)];
+
+
+ }
+
+passwordBox.value = password;
+
+}
+
+function copyPassword(){
+  passwordBox.select()
+  document.execCommand("copy");
+}
